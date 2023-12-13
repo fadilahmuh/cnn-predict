054d9f8bbc16cc0e54276ae0c1adbbbdb82b6e92
fadilahmuh <67127698+fadilahmuh@users.noreply.github.com>
2023-12-13 18:22:04 +0700

    lara

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/predict', function(){
+    // $response = Http::asForm()->get(env('API_URL') );
+    $response = (object) Http::asForm()->get(env('API_URL') )->json();
+    dd($response);
+    return $response;
+});
